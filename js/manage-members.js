e12dd5046ef93fb33997727fe9bf47c1395c5129
Chathura Madhusanka <madhusanka.slc@gmail.com>
2022-11-08 14:06:16 +0530

    complete manage member FE

--- js/manage-members.js
+++ js/manage-members.js
@@ -1,4 +1,5 @@
 const API_END_POINT='http://35.200.162.107:8080/lms/api';
+// const API_END_POINT='http://localhost:8080/lms/api';
 const pageSize = 8;
 let page = 1;
 
@@ -51,6 +52,11 @@ function getMembers(query=`${$('#txt-search').val()}`){
 
 function initPagination(totalMembers){
     const totalPages = Math.ceil(totalMembers / pageSize);
+    if(page>totalPages){
+       page=totalPages
+       getMembers();
+       return;
+    }        
     
     if (totalPages <= 1){
         $("#pagination").addClass('d-none');
@@ -120,10 +126,13 @@ $("#btn-new-member").click(()=> {
                 bootstrap.Modal(document.getElementById('frm-member-detail'));
 
     $("#frm-member-detail")
+    .removeClass('edit')
         .addClass('new')
         .on('shown.bs.modal', ()=> {
+            $('#txt-id,#txt-name,#txt-address,#txt-contact').attr('disabled',false).val('');
             $("#txt-name").focus();
         });
+
 
     frmMemberDetail.show();
 });
@@ -230,17 +239,190 @@ $('#frm-member-detail').on('hidden.bs.modal',()=>{
 });
 
 
+$('#tbl-members tbody').click(({target})=>{
+ 
+    if(!target) return;
+    // let rowElm=null;
+    let rowElm=target.closest('tr');;
+
+    // if(target instanceof HTMLTableRowElement ){
+    //     rowElm=target;
+
+    // }else if(target instanceof HTMLTableCellElement){
+    //     rowElm=target.parentElement;
+    // }else{
+    //     return;
+    // }
+  
+    // console.log($(rowElm.cells[0].innerText));
+    getMemberDetails($(rowElm.cells[0]).text());
+});
+
+
+async function getMemberDetails(memberId){
+
+    try{
+        const response=await fetch(`${API_END_POINT}/members/${memberId}`);
+        if(response.ok){
+            const member=await response.json();
+            console.log(member)
+
+                const frmMemberDetail = new  
+                bootstrap.Modal(document.getElementById('frm-member-detail'));
+               
+
+
+                $("#frm-member-detail")
+                .removeClass('new')
+                .removeClass('edit');
+
+                $('#txt-id').attr('disabled','true').val(member[0].id);
+                $('#txt-name').attr('disabled','true').val(member[0].name);
+                $('#txt-address').attr('disabled','true').val(member[0].address);
+                $('#txt-contact').attr('disabled','true').val(member[0].contact);
+              
+               
+
+            frmMemberDetail.show();
+
+        }else{
+            throw new Error(response.status);
+        }
+
+    }catch(error){
+        showToast('Failed to fetch the memebr details');
+
+    }
 
 
 
 
+    // const http=new XMLHttpRequest();
+    // http.addEventListener('readystatechange',()=>{
+    //     if(http.readyState==XMLHttpRequest.DONE){
+    //         if(http.status===200){
+    //             const member=JSON.parse(http.responseText);
+    //             console.log(http.responseText);
+    //             const frmMemberDetail = new  
+    //             bootstrap.Modal(document.getElementById('frm-member-detail'));
+               
+
+
+    //             $("#frm-member-detail")
+    //             .removeClass('new');
+
+    //             $('#txt-id').attr('disabled','true').val(member.id);
+    //             $('#txt-name').attr('disabled','true').val(member.name);
+    //             $('#txt-address').attr('disabled','true').val(member.address);
+    //             $('#txt-contact').attr('disabled','true').val(member.contact);
+              
+               
+
+    //         frmMemberDetail.show();
+
+    //         }else{
+    //             showToast('Failed to fetch the memebr details','error');
+    //         }
+    //     }
+
+    // });
+
+    // http.open('GET',`${API_END_POINT}/members/${memberId}`,true);
+    // http.send();
+}
 
 
 
 
+$('#btn-edit').click(()=>{
+    $('#frm-member-detail').addClass('edit');
+    $("#txt-name, #txt-address, #txt-contact").attr("disabled",false);
+});
+
+$('#btn-delete').click(async()=>{
+    $('#overlay').removeClass('d-none');
+
+    try{
+        const response=await fetch(`${API_END_POINT}/members/${$('#txt-id').val()}`,{method: "DELETE"});
+
+        if(response.status===204){
+            showToast('Member has been deleted successfully','success');
+            // const frmMemberDetail = new  
+            // bootstrap.Modal(document.getElementById('frm-member-detail'));
+            // frmMemberDetail.hide();
+
+            $('#btn-close').click();
+
+        }else{
+            throw new Error(response.status);
+        }
+
+    }catch(error){
+        showToast('Failed to delete member, try again');
+
+    }finally{
+        $('#overlay').addClass('d-none');
+    }
+});
 
 
 
+$('#btn-update').click(async()=>{
+
+    const name = $("#txt-name").val();
+    const address = $("#txt-address").val();
+    const contact = $("#txt-contact").val();
+    let validated = true;
+
+    $("#txt-name, #txt-address, #txt-contact").removeClass('is-invalid');
+
+    if (!/^\d{3}-\d{7}$/.test(contact)){
+        $("#txt-contact").addClass('is-invalid').select().focus();
+        validated = false;
+    }
+
+    if (!/^[A-Za-z0-9|,.:;#\/\\ -]+$/.test(address)){
+        $("#txt-address").addClass('is-invalid').select().focus();
+        validated = false;
+    }
+
+    if (!/^[A-Za-z ]+$/.test(name)){
+        $("#txt-name").addClass('is-invalid').select().focus();
+        validated = false;
+    }
+
+    if (!validated) return;
+
+    $('#overlay').removeClass('d-none');
+
+   try{
+    const response= await fetch(`${API_END_POINT}/members/${$('#txt-id').val()}`,
+    {
+        method: "PATCH",
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            id:$('#txt-id').val(),
+            name,address,contact
+        })
+
+    });
+    if(response.status===204){
+        showToast("Member has been updated",'success');
+    }else{
+        throw new Error(response.status);
+    }
+    
+
+   }catch(error){
+    showToast("Faild to update");
+   }finally{
+    $('#overlay').addClass('d-none');
+   }
+ 
+    
+});
 
 
 
